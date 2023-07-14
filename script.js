7bde9b8c6a8be680436bdfa26c33d42ae36134d1
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-14 09:56:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -27,22 +27,23 @@ const lookup = {
   X: "K",
   Y: "L",
   Z: "M",
+  " ":" ",
   "?": "?",
   ",": ",",
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+  let decodedArr = ""; // Your Result goes here
   for(var i = 0;i<encodedStr.length;i++){
       const character = encodedStr[i];
-	 decodedArr.push(lookup[character]);
+	 decodedArr+=lookup[character];
   }
   return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
- // console.log(rot13("SERR YBIR? NPPVBWBO"));
+ // console.log(rot13("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
 
 // Do not change this line
-window.rot13 = rot13;
+ window.rot13 = rot13;
